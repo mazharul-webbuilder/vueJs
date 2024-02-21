44554da0dac695c203a542858776e40dd65b6a0a
mazharul <rafi1998.bdr@gmail.com>
2024-02-21 16:44:18 +0600

    practices

---
+++ practices/day2/2/app.js
@@ -0,0 +1,25 @@
+const vm = Vue.createApp({
+    data(){
+        return{
+            number: 0
+        }
+    },
+    methods: {
+        increment: function (event) {
+            this.number++
+        },
+        decrement: function (event) {
+            if (this.number <= 0) {
+                window.alert('Can\'t decrement less than zero' )
+            } else {
+                this.number--
+            }
+        },
+        incrementNumber: function (msg, event) {
+            let inputNumber = event.target.value
+            if (inputNumber) {
+                this.number = inputNumber
+            }
+        }
+    }
+}).mount('#app')
