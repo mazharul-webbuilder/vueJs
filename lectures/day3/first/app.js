10f4ab202cc2278fb3a5ebe1dcec95110f23e7ff
Mazharul <mazharul.islam@durbar.live>
2024-02-22 13:02:32 +0600

    model modifier

---
+++ lectures/day3/first/app.js
@@ -0,0 +1,27 @@
+const vm = Vue.createApp({
+    data() {
+        return {
+            firstName: 'John',
+            middleName: '',
+            lastName: 'Doe',
+            url: 'https://www.google.com/',
+            raw_url: '<a href="https://www.google.com/">Raw Url Data</a>',
+            age: 16
+        }
+    },
+    methods: {
+        fullName() {
+            return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
+        },
+        increment(){
+            return this.age++
+        },
+        updateLastName(msg, event) {
+            console.log(msg)
+            this.lastName = event.target.value
+        },
+        updateMiddleName(event) {
+            this.middleName = event.target.value
+        }
+    }
+}).mount('#app')
