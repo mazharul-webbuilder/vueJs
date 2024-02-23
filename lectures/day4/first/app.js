7007df385f4e32bd7567e53f1238ec0d4f93107d
mazharul <rafi1998.bdr@gmail.com>
2024-02-23 12:24:46 +0600

    dynamic class

---
+++ lectures/day4/first/app.js
@@ -0,0 +1,16 @@
+const vm  = Vue.createApp({
+    data(){
+        return{
+            isPurple: false,
+            selectedColor: '',
+        }
+    },
+    computed: {
+        circle_classes(){
+            return {
+                purple: this.isPurple,
+
+            }
+        }
+    }
+}).mount('#app')
