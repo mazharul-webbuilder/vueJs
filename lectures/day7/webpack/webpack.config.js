0b6c12d4fd4121af05511c91552135c590f13a31
Mazharul <mazharul.islam@durbar.live>
2024-02-27 14:59:54 +0600

    webpack

---
+++ lectures/day7/webpack/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist'
+    },
+    /*Add extra feature to webpack*/
+    module: {
+        rules: [
+            {
+
+            }
+        ]
+    }
+}
