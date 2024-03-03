d752a60132644305b2c08e5b64e7e4f241c5a5b7
Mazharul <mazharul.islam@durbar.live>
2024-03-03 17:47:11 +0600

    u

--- practices/day10/slots/README.md
+++ practices/day10/slots/README.md
@@ -1,4 +1,4 @@
-# component
+# slots
 
 ## Project setup
 ```
---
+++ practices/day10/slots/src/components/Form.vue
@@ -0,0 +1,19 @@
+<template>
+  <form action="">
+    <div class="help">
+      <slot name="help"></slot>
+    </div>
+    <div class="field">
+      <slot name="fields"></slot>
+    </div>
+    <div class="buttons">
+      <slot name="buttons"></slot>
+    </div>
+    <h2>This are the defatult data</h2>
+  </form>
+</template>
+<script>
+export default {
+  name: 'FormComponent',
+}
+</script>
