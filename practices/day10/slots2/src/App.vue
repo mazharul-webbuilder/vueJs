0cbfc13f57e423baecf79e2e466dfba593417df2
Mazharul <mazharul.islam@durbar.live>
2024-03-03 18:00:24 +0600

    u

---
+++ practices/day10/slots2/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <app-form>
+    <template v-slot:help>
+      <p>{{ help }}</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="enter name">
+      <br>
+      <br>
+      <input type="email" placeholder="Enter email">
+      <br>
+      <br>
+    </template>
+    <template v-slot:buttons>
+      <button>My buttons</button>
+    </template>
+  </app-form>
+
+  <app-form>
+    <template v-slot:help>
+      <p>This is another help</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text"> <br><br>
+      <input type="text">
+      <br>
+      <br>
+    </template>
+
+    <template v-slot:buttons>
+      <button>Second button</button>
+    </template>
+  </app-form>
+</template>
+
+<script>
+import AppForm from './components/Form.vue'
+  export default {
+    name: 'App',
+    components: {AppForm},
+    data(){
+      return{
+        help: "This is helped data from Parent components"
+      }
+    }
+  }
+</script>
---
+++ practices/day10/slots2/src/components/Form.vue
@@ -0,0 +1,20 @@
+<template>
+  <form action="">
+    <div class="help">
+      <slot name="help"></slot>
+    </div>
+    <div class="fields">
+      <slot name="fields"></slot>
+    </div>
+    <div class="buttons">
+      <slot name="buttons"></slot>
+    </div>
+    <p>this is from Form component</p>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'FormComponent'
+  }
+</script>
