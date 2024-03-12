22b7b01616021d2ea5264926452dd879dcea85c7
Mazharul <mazharul.islam@durbar.live>
2024-03-12 15:08:03 +0600

    up

---
+++ practices/quiz3/src/components/Result.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="result">
+    <div class="title">{{results[resultIndex].title}}</div>
+    <div class="desc">{{results[resultIndex].desc}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ResultComponent",
+  props: ['results', 'totalCorrect'],
+  computed: {
+    resultIndex(){
+      console.log(this.totalCorrect)
+      let i = 0
+      this.results.forEach((element, index) => {
+        if (element.min <= this.totalCorrect && element.max >= this.totalCorrect){
+          i = index;
+        }
+      })
+      return i
+    }
+  }
+}
+</script>
