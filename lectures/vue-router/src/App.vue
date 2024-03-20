f148833ba6870487f0f2dbd643084f73a302f4be
Mazharul <mazharul.islam@durbar.live>
2024-03-20 15:58:38 +0600

    practice vue router;

---
+++ lectures/vue-router/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link
+        v-for="destination in destinations"
+        :key="destination.id"
+        :to="{name: destination.show, params: {id: destination.id, slug: destination.slug}}"
+    >{{destination.name}}</router-link>
+  </div>
+  <div class="container">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+export default {
+  name: "App",
+  data(){
+    return{
+      destinations: sourceData.destinations
+    }
+  }
+}
+</script>
