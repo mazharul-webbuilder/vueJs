4cc764e791c66e260453598b715907bd67500d8c
Mazharul <mazharul.islam@durbar.live>
2024-03-21 10:19:52 +0600

    practive vue router

---
+++ practices/vue-router27/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <app-navigation></app-navigation>
+  <div class="container">
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+import AppNavigation from "@/components/AppNavigation.vue";
+export default {
+  name: "App",
+  components: {
+    AppNavigation
+  },
+}
+</script>
