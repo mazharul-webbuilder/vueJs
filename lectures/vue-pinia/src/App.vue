5159a2f3d3bbd973fa39bc9bff9d562534159058
Mazharul <mazharul.islam@durbar.live>
2024-03-24 13:08:29 +0600

    pinia l

---
+++ lectures/vue-pinia/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Ecommerce Home page</h1>
+  <button>{{productQuantity}}</button>
+  <button>{{doubleOfProductQuantity}}</button>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
+</template>
+
+<script>
+import {mapState, mapActions} from "pinia";
+import {useCartStore} from "@/stores/CartStore.js";
+export default {
+  name: "App",
+  computed: {
+    ...mapState(useCartStore, {
+      productQuantity: "productQuantity",
+      doubleOfProductQuantity: "doubleOfProductQuantity"
+    })
+  },
+  methods: {
+    ...mapActions(useCartStore, {
+      increment: "increment",
+      decrement: "decrement",
+    })
+  }
+}
+</script>
