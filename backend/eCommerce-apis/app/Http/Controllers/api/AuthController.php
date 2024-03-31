8484d888826b92dafcd6018cd03afc308032d00d
Mazharul <mazharul.islam@durbar.live>
2024-03-31 13:31:41 +0600

    add

---
+++ backend/eCommerce-apis/app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Register manage
+    */
+    public function register(RegisterRequest $request)
+    {
+        
+    }
+    
+    
+    /**
+     * Login manage
+    */
+    public function login(LoginRequest $request)
+    {
+        
+    }
+    
+    /**
+     * Logout
+    */
+    public function logout()
+    {
+        
+    }
+}
