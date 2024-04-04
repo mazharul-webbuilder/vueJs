03eef62139d368498c225e51c07320e59373377b
Mazharul <mazharul.islam@durbar.live>
2024-04-04 12:22:56 +0600

    error fix

--- backend/eCommerce-apis/app/Providers/AppServiceProvider.php
+++ backend/eCommerce-apis/app/Providers/AppServiceProvider.php
@@ -22,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Event::listen(OrderTakenEvent::class, OrderTakenListener::class);
+//        Event::listen(OrderTakenEvent::class, OrderTakenListener::class);
     }
 }
